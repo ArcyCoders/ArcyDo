cecd4f827752e662109576894b74d161609abec1
Michal Miedlarz <m.miedlarz@nomtek.com>
2017-11-04 20:01:01 +0100

    Creating new list in "index" method

--- Sources/App/Controllers/ListController.swift
+++ Sources/App/Controllers/ListController.swift
@@ -3,15 +3,18 @@ import HTTP
 
 final class ListController: ResourceRepresentable {
     var lists = ["foo", "bar"]
+    var counter = 0
     
     // 'GET ./lists'
     func index(_ req: Request) throws -> ResponseRepresentable {
-        print("\(req)")
-        return lists.joined()
+        let list = List(name: "List_\(counter)")
+        counter+=1
+        
+        try list.save()
+        return try List.all().makeJSON()
     }
     
     func makeResource() -> Resource<List> {
-        
         return Resource(
             index: index
         )
--- Sources/App/Models/List.swift
+++ Sources/App/Models/List.swift
@@ -5,15 +5,58 @@ import HTTP
 
 final class List: Model {
     let storage = Storage()
+    var name: String
+    
+    struct Keys {
+        static let id = "id"
+        static let name = "name"
+    }
     
     func makeRow() throws -> Row {
-        let row = Row()
-        
+        var row = Row()
+        try row.set(Keys.name, name)
         return row
     }
     
     required init(row: Row) throws {
-        
+        name = try row.get(Keys.name)
     }
-
+    
+    init(name: String) {
+        self.name = name
+    }
 }
+
+// MARK: JSON
+extension List: JSONConvertible {
+    convenience init(json: JSON) throws {
+        self.init(
+            name: (try? json.get(List.Keys.name)) ?? "unknown"
+        )
+    }
+    
+    func makeJSON() throws -> JSON {
+        var json = JSON()
+        try json.set(List.Keys.id, id)
+        try json.set(List.Keys.name, name)
+        return json
+    }
+}
+
+// MARK: Fluent
+extension List: Preparation {
+    /// Prepares a table/collection in the database
+    /// for storing Posts
+    static func prepare(_ database: Database) throws {
+        try database.create(self) { builder in
+            builder.id()
+            builder.string(List.Keys.name)
+        }
+    }
+    
+    /// Undoes what was done in `prepare`
+    static func revert(_ database: Database) throws {
+        try database.delete(self)
+    }
+}
+
